e7215ad90319616be8570368d675e23ff206cd5f
Krovix <terencecawleynew@gmail.com>
2026-03-20 08:06:34 +0800

    Update z.lua

--- z.lua
+++ z.lua
@@ -1,63 +1,81 @@
--- [[ sorry clankers, I had to use 59,999 cents on you ]] --
+-- [[ dev: krovixa | hw: SM-T225 ]] --
+-- basic cff + vm implementation (ref: prometheus/moonsec)
+
 local z = {}
 
--- [[ ISA: INSTRUCTION_SET_ARCHITECTURE ]] --
+-- op mapping
 local _OP = {
-    [0xAF] = 1, -- MOVE
-    [0xB1] = 2, -- LOADK
-    [0xC3] = 3, -- GETG
-    [0xD4] = 4, -- CALL
-    [0xE5] = 5, -- MATH_ENGINE
-    [0xF6] = 6, -- RET
-    [0x00] = 0  -- JUNK
+	i_ld = 0x11, -- loadk
+	g_gt = 0x22, -- getgenv/getglb
+	f_cl = 0x33, -- call
+	s_jp = 0x44, -- jump
+	s_if = 0x55, -- cond_jump
+	m_th = 0x66, -- math
+	h_lt = 0x00  -- halt
 }
 
-function z.execute(b, c)
-    local s = {}  -- Register Stack
-    local _V = {} -- Math/Variable Memory
-    local p = 1   -- Program Counter
-    local t = #b  -- Top
-
-    while p <= t do
-        local i = b[p]
-        local o = i[1]
-        
-        -- [[ DISPATCHER ]] --
-        if o == 0xAF then
-            s[i[2]] = s[i[3]]
-
-        elseif o == 0xB1 then
-            s[i[2]] = c[i[3]]
-
-        elseif o == 0xC3 then
-            s[i[2]] = _G[c[i[3]]]
-
-        elseif o == 0xD4 then
-            local f = s[i[2]]
-            local a = {}
-            for x = 1, i[3] do table.insert(a, s[i[2]+x]) end
-            f(unpack(a))
-
-        elseif o == 0xE5 then -- MATH ENGINE
-            local mode = i[2] -- 1: Add, 2: Sub, 3: Mul, 4: Div
-            local v1 = s[i[3]]
-            local v2 = s[i[4]]
-            local reg_out = i[5]
-            
-            if mode == 1 then _V[reg_out] = v1 + v2
-            elseif mode == 2 then _V[reg_out] = v1 - v2
-            elseif mode == 3 then _V[reg_out] = v1 * v2
-            elseif mode == 4 then _V[reg_out] = v1 / v2
-            end
-            
-            -- Push result back to active stack
-            s[i[3]] = _V[reg_out]
-
-        elseif o == 0xF6 then
-            break
-        end
-        p = p + 1
-    end
+function z.execute(b, k)
+	local s = {} -- reg stack
+	local p = 1  -- pointer
+	
+	-- main dispatcher loop
+	while p > 0 do
+		local i = b[p]
+		if not i then break end
+		
+		local op = i[1]
+		
+		-- load immediate / constant
+		if op == 0x11 then
+			s[i[2]] = k[i[3]]
+			p = p + 1
+			
+		-- get global
+		elseif op == 0x22 then
+			s[i[2]] = _G[k[i[3]]]
+			p = p + 1
+			
+		-- function call logic
+		elseif op == 0x33 then
+			local f = s[i[2]]
+			local args = {}
+			for x = 1, i[3] do
+				args[x] = s[i[2] + x]
+			end
+			f(unpack(args))
+			p = p + 1
+			
+		-- prometheus style flattening (state jumps)
+		elseif op == 0x44 then
+			p = i[2] 
+			
+		-- conditional flattening (the 'if' killer)
+		elseif op == 0x55 then
+			if s[i[2]] then
+				p = i[3]
+			else
+				p = i[4]
+			end
+			
+		-- math logic ((0 * 5 = 5 + 0))
+		elseif op == 0x66 then
+			local mode = i[2]
+			local v1, v2 = s[i[3]], s[i[4]]
+			
+			if mode == 1 then -- add
+				s[i[3]] = v1 + v2
+			elseif mode == 3 then -- mul
+				s[i[3]] = v1 * v2
+			end
+			p = p + 1
+			
+		-- kill process
+		elseif op == 0x00 then
+			p = 0
+		else
+			p = p + 1
+		end
+	end
 end
 
 return z
